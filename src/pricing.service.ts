15c6d9d8825478313c8b1aac78849f0edfe1657e
Samuel ROZE <samuel.roze@gmail.com>
2022-04-08 14:43:30 +0100

    Phone number might be empty

--- src/pricing.service.ts
+++ src/pricing.service.ts
@@ -4,7 +4,11 @@ import { prices } from './constants/prices';
 @Injectable()
 export class PricingService {
 
-  getPrice(phoneNumber: string) {
+  getPrice(phoneNumber?: string): number {
+    if (!phoneNumber) {
+      return 0;
+    }
+    
     const prefixes = Object.keys(prices);
     const prefix = prefixes.find(p => phoneNumber.startsWith(p));
     return prices[prefix];
