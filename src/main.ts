34d920f2c01abeee94b959d194a78a2fe0b967f1
Marin Godechot <marin.godechot@gmail.com>
2023-06-26 19:09:30 +0100

    add bulma css styles

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import { join } from 'path';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import * as hbs from "express-handlebars";
+import { AppModule } from "./app.module";
+import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
-
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.engine(
+    "hbs",
+    hbs({
+      extname: "hbs",
+      defaultLayout: "main",
+      layoutsDir: join(__dirname, "..", "views/layouts"),
+    })
+  );
+  app.setViewEngine("hbs");
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
